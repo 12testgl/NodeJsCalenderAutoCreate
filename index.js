7a79d1df5385db8f4894aaff40d6afb6123de799
12testgl <109276884+12testgl@users.noreply.github.com>
2024-08-19 11:37:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,89 @@
+//index.js
+const express = require("express");
+const { google } = require("googleapis");
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Define the scope of access for the Google Calendar API.
+const scopes = ["https://www.googleapis.com/auth/calendar"];
+
+// OAuth 2 configuration
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
+
+const calendar = google.calendar({
+  version: "v3",
+  auth: oauth2Client,
+});
+
+const event = {
+  summary: "Tech Talk with Test",
+  location: "Google Meet",
+
+  description: "Demo event for Test's Blog Post.",
+  start: {
+    dateTime: "2024-03-14T19:30:00+05:30",
+    timeZone: "Asia/Kolkata",
+  },
+  end: {
+    dateTime: "2024-03-14T20:30:00+05:30",
+    timeZone: "Asia/Kolkata",
+  },
+  colorId: 1,
+  conferenceData: {
+    createRequest: {
+      requestId: uuid(),
+    },
+  },
+
+  attendees: [{ email: "testUser2020@gmail.com" }],
+};
+
+app.get("/auth", (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+  res.redirect(url);
+});
+
+app.get("/auth/redirect", async (req, res) => {
+  const { tokens } = await oauth2Client.getToken(req.query.code);
+  oauth2Client.setCredentials(tokens);
+  res.send("Authentication successful! Please return to the console.");
+});
+
+app.get("/create-event", async (req, res) => {
+  try {
+    const result = await calendar.events.insert({
+      calendarId: "primary",
+      auth: oauth2Client,
+      conferenceDataVersion: 1,
+      sendUpdates: "all",
+      resource: event,
+    });
+
+    res.send({
+      status: 200,
+      message: "Event created",
+      link: result.data.hangoutLink,
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+// https://dev.to/arindam_1729/i-built-an-event-scheduler-in-nodejs-using-google-calendar-api-4c5b?ref=dailydev
